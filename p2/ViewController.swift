5d2343e0c59315edfcd71f0c8afbbb7318fec671
Usama Ali <usamaali8981@gmail.com>
2021-08-10 19:43:06 +0500

    aa

---
+++ p2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  p2
+//
+//  Created by Usama Ali on 10/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
